31a1da777431138e7aa8ce13b58d9d65a0f7384a
Mirko Mälicke <mirko@hydrocode.de>
2021-04-08 14:18:18 +0200

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+// This is mandatory because App.css loads the ant-design css globally
 import './App.css';
 
+// ------------------------------------------------------------
+//            Firebase setup
+// ------------------------------------------------------------
+// this is the config, to make the SDK find the correct backend
+import config from './firebase-config';
+
+// Context provider to access the SDK
+import { FirestoreProvider } from '@react-firebase/firestore';
+
+// load the firebase SDK itself at App level
+import firebase from 'firebase/app';
+import 'firebase/firestore';        // this is needed once, so that firebase.app().firestore() is not null anymore
+
+// THIS is only development and will be removed later
+// HERE the react-router is needed to load one of the layouts:
+// - Page layout
+// - Permalink layout
 import { PageHeader } from 'antd';
 
 function App() {
   return (
-    <React.Fragment>
+    <FirestoreProvider {...config} firebase={firebase}>
       <PageHeader title="Hello From Kinamo" />
       <h3>there is no content so far</h3>
-    </React.Fragment>
-    
-
+    </FirestoreProvider>
   );
 }
 
---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUqZ8JRHeidevtbDUwlwyw3Gww3O703_s",
+    authDomain: "kinamo-3ec0e.firebaseapp.com",
+    projectId: "kinamo-3ec0e",
+    storageBucket: "kinamo-3ec0e.appspot.com",
+    messagingSenderId: "830223469999",
+    appId: "1:830223469999:web:6f232d85befa495d6e335a",
+    databaseURL: ""
+  };
+
+  export default firebaseConfig;
