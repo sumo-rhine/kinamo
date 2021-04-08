32f3373af8077777894ddfb49b22124f29a7b606
Mirko Mälicke <mirko@hydrocode.de>
2021-04-08 13:53:32 +0200

    Typescript setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import { PageHeader } from 'antd';
+
+function App() {
+  return (
+    <React.Fragment>
+      <PageHeader title="Hello From Kinamo" />
+      <h3>there is no content so far</h3>
+    </React.Fragment>
+    
+
+  );
+}
+
+export default App;
