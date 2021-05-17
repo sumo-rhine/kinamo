37702cce29db10183c2c350178f01607869fea78
JesJehle <jesjehle@gmx.de>
2021-05-17 11:27:03 +0200

    created Landing page installed material-icons

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+// import MapBanner from "../components/MapBanner";
+import { Box } from "@material-ui/core";
+
+const LandingPage: React.FC = () => {
+  return (
+    <Box>
+      <Header></Header>
+      {/* <MapBanner></MapBanner> */}
+    </Box>
+  );
+};
+
+export default LandingPage;
