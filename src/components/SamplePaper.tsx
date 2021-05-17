7edb5842c1707060c4193f923ad48aa0d1316f4b
JesJehle <jesjehle@gmx.de>
2021-05-17 10:00:12 +0200

    replaces sampleIndicatorCard with Sample paper

---
+++ src/components/SamplePaper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+
+const SamplePaper: React.FC = () => {
+  return (
+    <Paper elevation={1}>
+      <h1>Sample Page</h1>
+    </Paper>
+  );
+};
+
+export default SamplePaper;
--- src/pages/SamplePage.tsx
+++ src/pages/SamplePage.tsx
@@ -1,38 +1,11 @@
-import { Col, PageHeader, Row } from "antd";
 import React from "react";
-import SampleIndicatorCard from "../components/SampleIndicatorCard";
-
+import SamplePaper from "../components/SamplePaper";
 const SamplePage: React.FC = () => {
-    
-    return (
+  return (
     <React.Fragment>
-        <PageHeader title="Sample Component" />
-        <h1>Sample content</h1>
-        <h3>Section with child <code>component</code></h3>
-        
-        {/* Here, we would consume a page-wide state or an application wide setting, which city to show */}
-        <Row gutter={16}>
-            
-            <Col className="gutter-row" span={24} md={12} lg={6}>
-                <SampleIndicatorCard city="Freiburg im Breisgau" indicator="pedestrian" />
-            </Col>
-            
-            <Col className="gutter-row" span={24} md={12} lg={6}>
-                <SampleIndicatorCard city="Freiburg im Breisgau" indicator="pedestrian.shareGreen" />  
-            </Col>
-            
-            <Col className="gutter-row" span={24} md={12} lg={6}>
-                <SampleIndicatorCard city="Freiburg im Breisgau" indicator="pedestrian.carFreeArea" />
-            </Col>
-
-            <Col className="gutter-row" span={24} md={12} lg={6}>
-                <SampleIndicatorCard city="Freiburg im Breisgau" indicator="pedestrian.NotAnIndex" />
-            </Col>
-
-        </Row>
-        
-
+      <SamplePaper />
     </React.Fragment>
-)};
+  );
+};
 
 export default SamplePage;
