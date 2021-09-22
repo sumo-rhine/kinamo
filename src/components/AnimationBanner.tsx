92b278e6da7ec093880f7ca0908cfe69a28cc886
JesJehle <jesjehle@gmx.de>
2021-09-22 13:41:31 +0200

    first implementation of Map Banner component

--- src/components/AnimationBanner.tsx
+++ src/components/AnimationBanner.tsx
@@ -1,29 +1,56 @@
-import { makeStyles } from "@material-ui/core";
-
+import { CardMedia, makeStyles } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
+import map from "../banner/102.png";
 const useStyles = makeStyles({
+  font: {
+    position: "absolute",
+    top: "30%",
+    width: "100%",
+    paddingLeft: "10%",
+    textAlign: "left",
+    color: "white",
+    backgroundColor: "none",
+    // fontFamily: "Roboto,
+  },
   banner: {
-    display: `flex`,
-    justifyContent: `center`,
-    width: `2000px`,
+    position: "relative",
+    perspective: "1200px",
+    "-webkit-perspective": "1200px",
+    overflow: "hidden",
+
+    // display: `flex`,
+    // justifyContent: `center`,
+    // width: `4000px`,
+    height: `700px`,
     backgroundColor: `rgb(0, 0, 0)`,
   },
   css_transform: {
-    //   transform: "rotateX(20deg) rotateY(-1deg) rotateZ(180deg)",
-    transformOrigin: "55% 50% 200px",
-    animation: "animation 50s linear infinite normal",
+    position: "relative",
+    top: "-300px",
+
+    // display: `flex`,
+    // justifyContent: `center`,
+    // alignContent: `flex-start`,
+
+    // width: `4000px`,
+    height: `1200px`,
+    // overflow: "hidden",
+
+    transformOrigin: "55% 50% 0px",
+    animation: `$mapRotation 50s linear infinite normal`,
   },
-  "@keyframes animation": {
+  "@keyframes mapRotation": {
     "0%": {
-      transform: "rotateX(20deg) rotateY(-1deg) rotateZ(-180deg)",
-      filter: "saturate(50%) contrast(50%) hue-rotate(-180deg)",
+      transform: "rotateX(10deg) rotateY(-1deg) rotateZ(-180deg)",
+      filter: "saturate(0%) contrast(100%) hue-rotate(-180deg)",
     },
     "50%": {
       transform: "rotateX(50deg) rotateY(-4deg) rotateZ(0deg)",
-      filter: "saturate(100%) contrast(100%) hue-rotate(180deg)",
+      filter: "saturate(50%) contrast(50%) hue-rotate(180deg)",
     },
     "100%": {
-      transform: "rotateX(20deg) rotateY(-1deg) rotateZ(180deg)",
-      filter: "saturate(50%) contrast(50%) hue-rotate(-180deg)",
+      transform: "rotateX(10deg) rotateY(-1deg) rotateZ(180deg)",
+      filter: "saturate(0%) contrast(100%) hue-rotate(-180deg)",
     },
   },
 });
@@ -31,13 +58,20 @@ const useStyles = makeStyles({
 const AnimationBanner = () => {
   const classes = useStyles();
   return (
-    <div className={classes.banner}>
-      <object
-        className={classes.css_transform}
-        data="../../assets/banner/102.png"
-        type=""
-      ></object>
-    </div>
+    <Box className={classes.banner}>
+      <CardMedia image={map} className={classes.css_transform} />
+      <Box className={classes.font}>
+        <Typography gutterBottom variant="h1" component="h1">
+          Explore Cities
+        </Typography>
+        <Typography gutterBottom variant="h3" component="h3">
+          in the Upper Rhine Region
+        </Typography>
+        <Typography gutterBottom variant="h5" component="h5">
+          Follow the the transition to sustainable mobility
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
