5beec70b4f530dd236f4f527bf27bcf90cdb1cf8
JesJehle <jesjehle@gmx.de>
2021-09-24 09:54:54 +0200

    some fixes

--- src/components/City-Table/City-Table-Cell.tsx
+++ src/components/City-Table/City-Table-Cell.tsx
@@ -2,39 +2,47 @@ import TableCell from "@mui/material/TableCell";
 import { IndicatorStat } from "./City-Table";
 
 interface CityTableCellProps {
-    value: number;
-    showOption: "number" | "bar";
-    stats: IndicatorStat;
-    align?: "left" | "right" | "center";
+  value: number;
+  showOption: "number" | "bar";
+  stats: IndicatorStat;
+  align?: "left" | "right" | "center";
 }
 
 const CityTableCell: React.FC<CityTableCellProps> = (props) => {
-    let cell: any;
-    switch (props.showOption) {
-        case 'bar':
-            const width =  (props.value - props.stats.min) / (props.stats.max - props.stats.min) * 100
-            cell = (
-                <div style={{width: '100%', height: '0.9rem'}}>
-                        <div style={{
-                            backgroundColor: 'green',
-                            height: '100%',
-                            width: `${width.toFixed(0)}%`,
-                            borderRadius: '.3rem'
-                        }}>
-                        </div>
-                    </div>
-            );
-            break;
-    
-        default:
-            cell = props.value.toFixed(1);
-            break;
-    }
-    return (
-        <TableCell align={props.align ? props.align : "right"}>
-            { cell }
-        </TableCell>
-    )
-}
+  let cell: any;
+  switch (props.showOption) {
+    case "bar":
+      const width =
+        ((props.value - props.stats.min) /
+          (props.stats.max - props.stats.min)) *
+        100;
+      cell = (
+        <div
+          style={{
+            width: "70%",
+            height: "0.5rem",
+            backgroundColor: "lightgrey",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: "green",
+              height: "100%",
+              width: `${width.toFixed(0)}%`,
+              // borderRadius: '.3rem'
+            }}
+          ></div>
+        </div>
+      );
+      break;
+
+    default:
+      cell = props.value.toFixed(1);
+      break;
+  }
+  return (
+    <TableCell align={props.align ? props.align : "right"}>{cell}</TableCell>
+  );
+};
 
 export default CityTableCell;
--- src/components/City-Table/City-Table.tsx
+++ src/components/City-Table/City-Table.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import sortBy from "lodash.sortby";
 
-import { Paper } from "@mui/material";
+import { Paper, Box } from "@mui/material";
 import TableContainer from "@mui/material/TableContainer";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
@@ -15,27 +15,26 @@ import { AppState } from "../../models/AppState";
 import { City } from "../../models/FullDataset";
 import CityTableCell from "./City-Table-Cell";
 
-
 // define the HeadCell interface and object array
 interface HeadCell {
-    id: string;
-    label: string;
+  id: string;
+  label: string;
 }
 
-
 const HEADCELLS: HeadCell[] = [
-    { id: 'city', label: 'City' },
-    { id: 'walkability', label: 'Walkability' },
-    { id: 'bikeability', label: 'Bikeability' },
-    { id: 'car_integration', label: 'Car Integration' },
-    { id: 'noise_pollution', label: 'Noise Pollution'},
-    { id: 'land_use', label: 'Landuse'},
-    { id: 'traffic_safety', label: 'Traffic Safety'},
-    { id: 'emissions', label: 'Emissions'},
-    { id: 'cross_border', label: 'Border Patrol'},
-    { id: 'functional_diversity', label: 'Functional Diversity'},
-    { id: 'accessibility', label: 'Accessibility'},
-    { id: 'behavior', label: 'Behavior'}
+  { id: "city", label: "City" },
+  { id: "walkability", label: "Walkability" },
+  { id: "bikeability", label: "Bikeability" },
+  { id: "car_integration", label: "Car" },
+  { id: "public_transport", label: "Public Transport" },
+  { id: "noise_pollution", label: "Noise" },
+  { id: "land_use", label: "Landuse" },
+  { id: "traffic_safety", label: "Safety" },
+  { id: "emissions", label: "Emissions" },
+  { id: "cross_border", label: "Border Patrol" },
+  { id: "functional_diversity", label: "Functional Diversity" },
+  { id: "accessibility", label: "Accessibility" },
+  { id: "behavior", label: "Behavior" },
 ];
 
 interface FlatCityInfo {
@@ -44,14 +43,14 @@ interface FlatCityInfo {
 }
 
 export interface IndicatorStat {
-    min: number;
-    max: number;
-    median?: number;
-    mean?: number;
+  min: number;
+  max: number;
+  median?: number;
+  mean?: number;
 }
 
 interface IndicatorStats {
-    [indicator: string]: IndicatorStat
+  [indicator: string]: IndicatorStat;
 }
 
 interface CityTableProps {
@@ -60,109 +59,110 @@ interface CityTableProps {
 }
 
 const CityTable: React.FC<CityTableProps> = (props) => {
-    // define component state
-    const [order, setOrder] = useState<"asc" | "desc">('asc');
-    const [orderBy, setOrderBy] = useState<string>('city');
-    const [showOption, setShowOption] = useState<"number" | "bar">('bar');
+  // define component state
+  const [order, setOrder] = useState<"asc" | "desc">("asc");
+  const [orderBy, setOrderBy] = useState<string>("city");
+  const [showOption, setShowOption] = useState<"number" | "bar">("bar");
 
-    // transform the cities into the FlatCityInfo interface
-    // as the data needs to be handled by state to sort it, we need the 
-    // useEffect hook to load it only if props changed
-    // otherwise it will be reloaded on EVERY click
-    const [flatData, setFlatData] = useState<FlatCityInfo[]>([]);
-    const [stats, setStats] = useState<IndicatorStats>({});
+  // transform the cities into the FlatCityInfo interface
+  // as the data needs to be handled by state to sort it, we need the
+  // useEffect hook to load it only if props changed
+  // otherwise it will be reloaded on EVERY click
+  const [flatData, setFlatData] = useState<FlatCityInfo[]>([]);
+  const [stats, setStats] = useState<IndicatorStats>({});
 
-    useEffect(() => {
-        // on component mount sort by city asc
-        const transformed = sortBy(transformData(props.cities), f => f.city);
-        const statistics = indicatorStatistics(transformed)
+  useEffect(() => {
+    // on component mount sort by city asc
+    const transformed = sortBy(transformData(props.cities), (f) => f.city);
+    const statistics = indicatorStatistics(transformed);
 
-        // set the data the first and the stats the only time
-        setFlatData(transformed);
-        setStats(statistics);
+    // set the data the first and the stats the only time
+    setFlatData(transformed);
+    setStats(statistics);
 
-        // dev output
-        if (props.debug) console.log(`[CityTable] transformData:`, transformed)
-        if (props.debug) console.log(`[CityTable] indicatorStatistics:`, statistics)
-    }, [props])
+    // dev output
+    if (props.debug) console.log(`[CityTable] transformData:`, transformed);
+    if (props.debug)
+      console.log(`[CityTable] indicatorStatistics:`, statistics);
+  }, [props]);
 
-    // do the sorting
-    const sortHandler = (headerId: string) => {
-        // I guess using lodash is the best solution here
-        const orderedFlatData = sortBy(flatData, f => f[headerId]);
+  // do the sorting
+  const sortHandler = (headerId: string) => {
+    // I guess using lodash is the best solution here
+    const orderedFlatData = sortBy(flatData, (f) => f[headerId]);
 
-        // check the order direction
-        if (order === 'desc') orderedFlatData.reverse()
+    // check the order direction
+    if (order === "desc") orderedFlatData.reverse();
 
-        // update 
-        setFlatData(orderedFlatData);
+    // update
+    setFlatData(orderedFlatData);
+  };
+
+  // handle the sort request
+  const onSortClick = (clickedId: string) => {
+    // check if the current orderby was clicked
+    if (clickedId === orderBy) {
+      // flip direction
+      setOrder(order === "asc" ? "desc" : "asc");
+    } else {
+      // change order by and set to asc
+      setOrderBy(clickedId);
+      setOrder("asc");
     }
 
-    // handle the sort request
-    const onSortClick = (clickedId: string) => {
-        // check if the current orderby was clicked
-        if (clickedId === orderBy) {
-            // flip direction
-            setOrder(order === 'asc' ? 'desc' : 'asc');
-        } else {
-            // change order by and set to asc
-            setOrderBy(clickedId);
-            setOrder('asc');
-        }
+    // all set -> go for sorting
+    sortHandler(clickedId);
+  };
 
-        // all set -> go for sorting
-        sortHandler(clickedId);
-    }
-    
-    
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{width: '100%'}} size="small">
-                {/* HEADER */}
-                <TableHead>
-                    <TableRow>
-                        { HEADCELLS.map(headcell => {
-                            return (
-                                <TableCell align={headcell.id === 'city' ? "left": "right"}>
-                                    <TableSortLabel
-                                        active={headcell.id === 'walkablility'}
-                                        direction={headcell.id === orderBy ? order : 'asc'}
-                                        onClick={() => onSortClick(headcell.id)}
-                                    >
-                                        {headcell.label}
-                                    </TableSortLabel>
-                                </TableCell>
-                            );
-                        }) }
-                    </TableRow>
-                </TableHead>
+  return (
+    <Box pt={20}>
+      <TableContainer component={Paper}>
+        <Table sx={{ width: "100%" }} size="small">
+          {/* HEADER */}
+          <TableHead>
+            <TableRow>
+              {HEADCELLS.map((headcell) => {
+                return (
+                  <TableCell align={headcell.id === "city" ? "left" : "right"}>
+                    <TableSortLabel
+                      active={headcell.id === "walkablility"}
+                      direction={headcell.id === orderBy ? order : "asc"}
+                      onClick={() => onSortClick(headcell.id)}
+                    >
+                      {headcell.label}
+                    </TableSortLabel>
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          </TableHead>
 
-                {/* TABLE BODY */}
-                <TableBody>
-                    { flatData.map((city => {
-                        return (
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    <strong>{city.city}</strong>
-                                </TableCell>
-                                { HEADCELLS.slice(1).map(headcell => {
-                                    return (
-                                        <CityTableCell 
-                                            value={Number(city[headcell.id])}
-                                            showOption={showOption}
-                                            stats={stats[headcell.id]}
-                                        />
-                                    );
-                                }) }
-                            </TableRow>
-                        );
-                    })) }
-                </TableBody>
-            </Table> 
-        </TableContainer>
-    );
-}
-
+          {/* TABLE BODY */}
+          <TableBody>
+            {flatData.map((city) => {
+              return (
+                <TableRow>
+                  <TableCell component="th" scope="row">
+                    <strong>{city.city}</strong>
+                  </TableCell>
+                  {HEADCELLS.slice(1).map((headcell) => {
+                    return (
+                      <CityTableCell
+                        value={Number(city[headcell.id])}
+                        showOption={showOption}
+                        stats={stats[headcell.id]}
+                      />
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
 
 const transformData = (cities: City[]): FlatCityInfo[] => {
   return cities.map((city) => {
@@ -174,22 +174,28 @@ const transformData = (cities: City[]): FlatCityInfo[] => {
   });
 };
 
-
 const indicatorStatistics = (flat: FlatCityInfo[]): IndicatorStats => {
-    const stats: IndicatorStats = {};
+  const stats: IndicatorStats = {};
 
-    HEADCELLS.slice(1).forEach(header => {
-        const stat: IndicatorStat = {min: 99, max: 0};
-        
-        // min and max
-        stat.min = Number(flat.map(f => f[header.id]).reduce((prev, cur) => prev < cur ? prev : cur, 99));
-        stat.max = Number(flat.map(f => f[header.id]).reduce((prev, cur) => prev > cur ? prev : cur, 0));
+  HEADCELLS.slice(1).forEach((header) => {
+    const stat: IndicatorStat = { min: 99, max: 0 };
 
-        stats[header.id] = stat as IndicatorStat;
-    });
-    return stats;
+    // min and max
+    stat.min = Number(
+      flat
+        .map((f) => f[header.id])
+        .reduce((prev, cur) => (prev < cur ? prev : cur), 99)
+    );
+    stat.max = Number(
+      flat
+        .map((f) => f[header.id])
+        .reduce((prev, cur) => (prev > cur ? prev : cur), 0)
+    );
+
+    stats[header.id] = stat as IndicatorStat;
+  });
+  return stats;
 };
-
 
 const mapStateToProps = (state: AppState) => {
   return {
