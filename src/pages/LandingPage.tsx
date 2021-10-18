4d8129302df4570604eece4a306d5522b5d4edcb
JesJehle <jesjehle@gmx.de>
2021-10-18 09:32:31 +0200

    updated package.json

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header/Header";
 // import RandomAnim from "../components/AnimationBanner/AnimationBanner";
 import RandomAnimationBanner from "../components/AnimationBanner/RandomAnimationBanner";
 // import AnimationBanner from "../components/AnimationBanner/AnimationBanner";
-import CityCarousel from "../components/Carousel/Carousel";
+// import CityCarousel from "../components/Carousel/Carousel";
 import CityTable from "../components/City-Table/City-Table";
 import SelectCityTabs from "../components/Header/SelectCityTabs";
 import DividerOverview from "../components/DividerOverview";
