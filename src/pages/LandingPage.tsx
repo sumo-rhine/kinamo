e633966ea89ff2de7abf306a1b5f65f26c30af27
JesJehle <jesjehle@gmx.de>
2021-11-09 10:48:19 +0100

    added extra page for mobile

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,24 +6,98 @@ import CityTable from "../components/City-Table/City-Table";
 import DividerOverview from "../components/DividerOverview";
 import About from "../components/How-it-Works/HowItWorks";
 import CityStepper from "../components/Carousel/CityStepper";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
 
 const LandingPage: React.FC = () => {
   const [active, setActive] = useState(false);
+  const [windowWidth, setWindowWidth] = useState<undefined | number>();
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    // console.log(windowWidth);
+  }, [window.innerWidth]);
 
   return (
     <Box>
-      <div id="top" />
-      <Header
-        // value={active}
-        // setter={setActive}
-        cityName="wähle deine Kommune"
-      ></Header>
-      <RandomAnimationBanner></RandomAnimationBanner>
-      <CityStepper />
-      <About value={active} setter={setActive} />
-      <DividerOverview />
-      <CityTable />
+      {windowWidth! > 1400 ? (
+        <Box>
+          <div id="top" />
+          <Header
+            // value={active}
+            // setter={setActive}
+            cityName="wähle deine Kommune"
+          ></Header>
+          <RandomAnimationBanner></RandomAnimationBanner>
+          <CityStepper />
+          <About value={active} setter={setActive} />
+          <DividerOverview />
+          <CityTable />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            // alignItems: "center",
+            backgroundColor: "black",
+            color: "white",
+          }}
+        >
+          <Box>
+            <img
+              style={{
+                position: "absolute",
+                zIndex: 2,
+                // height: 800,
+                // overflow: "hidden",
+                // boxShadow: "0 2px 4px silver",
+                // left: 40,
+                filter: "blur(1px)",
+                opacity: 0.3,
+              }}
+              alt="img"
+              src="/assets/thumbnail/102_crop.png"
+            />
+          </Box>
+          <Box sx={{}}>
+            <Typography
+              // variant="subtitle2"
+              mt={10}
+              m={6}
+              align="center"
+              variant="h6"
+              // color="text.secondary"
+              fontWeight="fontWeightLight"
+            >
+              Sie sind auf der KINaMo Webseite, die Applikation befindet
+              momentan noch in der Entwicklung und ist noch nicht für mobile
+              Geräte optimiert.
+            </Typography>
+            <Typography
+              m={6}
+              // variant="subtitle2"
+              align="center"
+              variant="h4"
+              // color="text.secondary"
+              fontWeight="fontWeightLight"
+            >
+              Bitte besuchen sie die Seite mit einem Desktop Gerät erneut.
+            </Typography>
+            <Typography
+              m={6}
+              // variant="subtitle2"
+              align="center"
+              variant="h6"
+              // color="text.secondary"
+              fontWeight="fontWeightLight"
+            >
+              Danke für ihr Verständnis
+            </Typography>
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
