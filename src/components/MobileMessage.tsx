950b198007f97fda85a8ff9b588a1b3cf4717597
JesJehle <jesjehle@gmx.de>
2021-11-12 17:42:29 +0100

    refectored mobile message

---
+++ src/components/MobileMessage.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "../components/Header/Header";
+import RandomAnimationBanner from "../components/AnimationBanner/RandomAnimationBanner";
+import CityTable from "../components/City-Table/City-Table";
+import XDataTable from "../components/City-Table/x-Data-Grid";
+import DividerOverview from "../components/DividerOverview";
+import About from "../components/How-it-Works/HowItWorks";
+import CityStepper from "../components/Carousel/CityStepper";
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import Footer from "../components/Footer/Footer";
+
+const MobileMessage = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        backgroundColor: "black",
+        color: "white",
+      }}
+    >
+      <Box>
+        {/* <img
+          style={{
+            position: "absolute",
+            zIndex: 2,
+            // height: 800,
+            // overflow: "hidden",
+            // boxShadow: "0 2px 4px silver",
+            // left: 40,
+            filter: "blur(1px)",
+            opacity: 0.3,
+          }}
+          alt="img"
+          src="/assets/thumbnail/102_crop.png" */}
+        {/* /> */}
+      </Box>
+      <Box sx={{}}>
+        <Typography
+          mt={10}
+          m={6}
+          align="center"
+          variant="h6"
+          fontWeight="fontWeightLight"
+        >
+          Sie sind auf der KINaMo Webseite, die Applikation befindet momentan
+          noch in der Entwicklung und ist noch nicht für mobile Geräte
+          optimiert.
+        </Typography>
+        <Typography
+          m={6}
+          align="center"
+          variant="h4"
+          fontWeight="fontWeightLight"
+        >
+          Bitte besuchen sie die Seite mit einem Desktop Gerät erneut.
+        </Typography>
+        <Typography
+          m={6}
+          align="center"
+          variant="h6"
+          fontWeight="fontWeightLight"
+        >
+          Danke für ihr Verständnis
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default MobileMessage;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,6 +10,7 @@ import CityStepper from "../components/Carousel/CityStepper";
 import { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import Footer from "../components/Footer/Footer";
+import MobileMessage from "../components/MobileMessage";
 const LandingPage: React.FC = () => {
   const [active, setActive] = useState(false);
   const [windowWidth, setWindowWidth] = useState<undefined | number>(1500);
@@ -38,61 +39,7 @@ const LandingPage: React.FC = () => {
           <Footer />
         </Box>
       ) : (
-        <Box
-          sx={{
-            width: "100%",
-            height: "100vh",
-            display: "flex",
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <Box>
-            <img
-              style={{
-                position: "absolute",
-                zIndex: 2,
-                // height: 800,
-                // overflow: "hidden",
-                // boxShadow: "0 2px 4px silver",
-                // left: 40,
-                filter: "blur(1px)",
-                opacity: 0.3,
-              }}
-              alt="img"
-              src="/assets/thumbnail/102_crop.png"
-            />
-          </Box>
-          <Box sx={{}}>
-            <Typography
-              mt={10}
-              m={6}
-              align="center"
-              variant="h6"
-              fontWeight="fontWeightLight"
-            >
-              Sie sind auf der KINaMo Webseite, die Applikation befindet
-              momentan noch in der Entwicklung und ist noch nicht für mobile
-              Geräte optimiert.
-            </Typography>
-            <Typography
-              m={6}
-              align="center"
-              variant="h4"
-              fontWeight="fontWeightLight"
-            >
-              Bitte besuchen sie die Seite mit einem Desktop Gerät erneut.
-            </Typography>
-            <Typography
-              m={6}
-              align="center"
-              variant="h6"
-              fontWeight="fontWeightLight"
-            >
-              Danke für ihr Verständnis
-            </Typography>
-          </Box>
-        </Box>
+        <MobileMessage />
       )}
     </Box>
   );
