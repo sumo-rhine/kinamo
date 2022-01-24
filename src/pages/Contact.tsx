efb158f895f9695ec187425dfe0e4d04ebd5eee1
JesJehle <jesjehle@gmx.de>
2022-01-24 12:47:04 +0100

    content of contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,20 +11,43 @@ const Contact = () => {
         <h4>
           Professur für Fernerkundung und Landschaftsinformationssysteme - FeLIS
         </h4>
-        <p>Tennenbacherstr. 4</p>
-        <p>D-79106 Freiburg </p>
-        <p>Tel. +49-(0)761-203-3694</p>
-        <p>Fax. +49-(0)761-203-3701</p>
-        <p>E-mail: ferninfo@felis.uni-freiburg.de</p>
+        <Box pl={2}>
+          <p>Tennenbacherstr. 4</p>
+          <p>D-79106 Freiburg </p>
+          <p>Tel. +49-(0)761-203-3694</p>
+          <p>Fax. +49-(0)761-203-3701</p>
+          <Link
+            //   sx={{ color: "white" }}
+            href="mailto:ferninfo@felis.uni-freiburg.de"
+          >
+            ferninfo@felis.uni-freiburg.de
+          </Link>
+        </Box>
 
         <h4>Prof. Dr. Barbara Koch</h4>
-        <p>Projektleitung Felis</p>
-        <p>E-mail: barbara.koch@uni-freiburg.de</p>
+        <Box pl={2}>
+          <p>Projektleitung Felis</p>
+          <Link
+            //   sx={{ color: "white" }}
+            href="mailto:barbara.koch@uni-freiburg.de"
+          >
+            barbara.koch@uni-freiburg.de
+          </Link>
+        </Box>
 
-        <h4>Janusch Vajna-Jehle</h4>
-        <p>Entwicklung und Umsetzung</p>
-        <p>E-mail: janusch.jehle@uni-freiburg.de</p>
-        <a href="mailto:janusch.jehle@uni-freiburg.de"></a>
+        <h4>MSc. Janusch Vajna-Jehle</h4>
+        <Box pl={2}>
+          <p>Wissenschaftlicher Mitarbeiter</p>
+          <p>Entwicklung und technische Umsetzung</p>
+          {/* <p>E-mail: janusch.jehle@uni-freiburg.de</p> */}
+          <Link
+            //   sx={{ color: "white" }}
+            href="mailto:janusch.jehle@uni-freiburg.de"
+          >
+            janusch.jehle@uni-freiburg.de
+          </Link>
+        </Box>
+        {/* <a href="mailto:janusch.jehle@uni-freiburg.de"></a> */}
       </Box>
     </Box>
   );
