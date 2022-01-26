107eff1bacbb8775b8db8ff6e00eefa44154e728
JesJehle <jesjehle@gmx.de>
2022-01-26 10:04:48 +0100

    formatted impressum

--- src/pages/Impressum.tsx
+++ src/pages/Impressum.tsx
@@ -6,28 +6,66 @@ const Impressum = () => {
     <Box>
       <Header cityName="wähle deine Kommune"></Header>
       <Box ml={10}>
-        
-
-      <h1>Impressum</h1>
-      <h2>Angaben gemäß § 5 TMG:</h2>
-      <p>hydrocode Reid &amp; Mälicke GbR<br /></p>
-      <h3>Postanschrift:</h3>
-      <p>Mombertstr 11<br />76131 Karlsruhe<br /></p>
-      <h3>Kontakt:</h3>
-      <p>Telefon: +4915156169657<br />E-Mail: mirko@hydrocode.de</p>
-      <p></p><h3>Vertreten durch:</h3>
-      <p>Lucas Reid, Mirko Mälicke<br /></p>
-      <p></p><p><br /></p>
-      <p></p><h2>Hinweise zur Website</h2>
-      <p></p><h3>Urheberrechtliche Hinweise</h3>
-      <p>Lucas Reid
-      <br />Mirko Mälicke<br /></p>
-      <p></p><h2>Information gemäß § 36 VSBG</h2>
-      <p>Gemäß § 36 VSBG (Verbraucherstreitbeilegungsgesetz – Gesetz über die alternative Streitbeilegung in Verbrauchersachen) erklärt der Betreiber dieser Website:</p>
-      <p>Wir sind weder bereit noch verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-      <p></p><p><em>Das Impressum wurde mit dem <a href="https://www.activemind.de/datenschutz/impressums-generator/">Impressums-Generator der activeMind AG</a> erstellt.</em></p>
-
-
+        <h1>Impressum</h1>
+        <h2>Angaben gemäß § 5 TMG:</h2>
+        <p>
+          hydrocode Reid &amp; Mälicke GbR
+          <br />
+        </p>
+        <h3>Postanschrift:</h3>
+        <p>
+          Mombertstr 11
+          <br />
+          76131 Karlsruhe
+          <br />
+        </p>
+        <h3>Kontakt:</h3>
+        <p>
+          Telefon: +4915156169657
+          <br />
+          E-Mail: mirko@hydrocode.de
+        </p>
+        <p></p>
+        <h3>Vertreten durch:</h3>
+        <p>
+          Lucas Reid, Mirko Mälicke
+          <br />
+        </p>
+        <p></p>
+        <p>
+          <br />
+        </p>
+        <p></p>
+        <h2>Hinweise zur Website</h2>
+        <p></p>
+        <h3>Urheberrechtliche Hinweise</h3>
+        <p>
+          Lucas Reid
+          <br />
+          Mirko Mälicke
+          <br />
+        </p>
+        <p></p>
+        <h2>Information gemäß § 36 VSBG</h2>
+        <p>
+          Gemäß § 36 VSBG (Verbraucherstreitbeilegungsgesetz – Gesetz über die
+          alternative Streitbeilegung in Verbrauchersachen) erklärt der
+          Betreiber dieser Website:
+        </p>
+        <p>
+          Wir sind weder bereit noch verpflichtet, an Streitbeilegungsverfahren
+          vor einer Verbraucherschlichtungsstelle teilzunehmen.
+        </p>
+        <p></p>
+        <p>
+          <em>
+            Das Impressum wurde mit dem{" "}
+            <a href="https://www.activemind.de/datenschutz/impressums-generator/">
+              Impressums-Generator der activeMind AG
+            </a>{" "}
+            erstellt.
+          </em>
+        </p>
       </Box>
     </Box>
   );
